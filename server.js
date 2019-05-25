700588f0da0ebbb1431837044ec4e672255c1377
cjropero <cjavieramaya@gmail.com>
2019-05-25 06:47:26 -0500

    (feat) => changes no execute fix @cjropero

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(bodyParser.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var users = require("./routes/Users")
+var Users = require("./routes/Users")
 
-app.use("/users", Users);
+app.use("/users", Users)
 
 app.listen(port, () => {
-  console.log(`Server started on port {$port}`);
-});
+  console.log("Server started on port" + port)
+})
